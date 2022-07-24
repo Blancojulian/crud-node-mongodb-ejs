f37d9d90413b2514afbf23df72b43b9dbaa52cd1
Blancojulian <julianblanco963@hotmail.com>
2022-07-24 05:33:15 -0300

    heroku

--- routes/alumnos.js
+++ routes/alumnos.js
@@ -5,7 +5,7 @@ const alumnoController = require("../controllers/alumnoController.js");
 
 router.get('/', alumnoController.getAllAlumnos)
     .post('/crear', alumnoController.createAlumno)
-    .put('/editar', alumnoController.updateAlumno)
+    .post('/editar', alumnoController.updateAlumno)
     .delete('/eliminar/:id', alumnoController.deleteAlumno);
 
 module.exports = router;
